9c286fced8b41f70b702d3296317542ca8b55b2f
liudong <liudong7747@163.com>
2022-01-07 16:30:47 +0800

    '.'

---
+++ src/views/Home.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
+import _ from "lodash";
+
+export default {
+  name: "Home",
+  components: {
+    HelloWorld,
+  },
+  data() {
+    return {
+      temp1: {
+        name: "zhangsan",
+        children: [
+          {
+            age: 20,
+          },
+        ],
+      },
+      temp2: {
+        name: "zhangsan",
+        children: [
+          {
+            age: 20,
+            children: [
+              {
+                sex: 1,
+              },
+            ],
+          },
+        ],
+      },
+    };
+  },
+  methods: {
+    fn() {
+      console.log(_.isEqual(this.temp1, this.temp2));
+    },
+  },
+  created() {
+    this.fn();
+  },
+};
+</script>
